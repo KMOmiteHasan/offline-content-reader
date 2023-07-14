cc0df4e865a9413998074dba6828d009a369a6d4
KMOmiteHasan <126360771+KMOmiteHasan@users.noreply.github.com>
2023-07-15 05:25:35 +0600

    Add files via upload

--- vendors/js/script.js
+++ vendors/js/script.js
@@ -3,14 +3,18 @@ const contentCardContainer = document.querySelector(
 );
 const searchInput = document.querySelector("[data-search]");
 let contents = document.querySelectorAll(".card h2");
+let noContent = document.querySelector(".no-card");
 
 searchInput.addEventListener("input", (e) => {
   const value = e.target.value.toLowerCase();
-  console.log(value);
-  console.log(contents);
   contents.forEach((content) => {
     const isVisible = content.innerHTML.toLowerCase().includes(value);
     content.parentElement.classList.toggle("hide", !isVisible);
-    console.log(contentCardContainer.childNodes);
   });
+  noContent.classList.add("show");
+  for (let i = 0; i < contents.length; i++) {
+    if (!contents[i].parentElement.classList.contains("hide")) {
+      noContent.classList.remove("show");
+    }
+  }
 });
